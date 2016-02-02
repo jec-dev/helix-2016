69922d91ddb00d8afb2dc2d1988f3fb9ea9a9c5c
zsharique <shariquezia94@gmail.com>
2016-02-02 11:49:49 +0530

    model changes

--- app/views.py
+++ app/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.contrib import messages
 from django.shortcuts import render, render_to_response
 from django.template.loader import get_template
 from django.template import Context, RequestContext
@@ -13,10 +14,20 @@ from django.contrib.auth.forms import UserCreationForm
 from django.core.urlresolvers import reverse
 from django.utils.timezone import utc
 from django.contrib.auth.decorators import login_required
+from app.forms import QueryForm
 
 def landing_view(request):
-    name = 'Foo Bar'
-    context = RequestContext(request)
-    t = get_template('index.html')
-    return render_to_response('landing.html', context)
+	name = 'Foo Bar'
+	context = RequestContext(request)
+	t = get_template('index.html')
+	form = QueryForm()
 
+	if request.POST:
+		form = QueryForm(request.POST)
+		if form.is_valid():
+			query = form.save()
+		else:
+			print 'not valid', form.errors
+
+		#return render_to_response('landing.html','form':form,context_instance=RequestContext(request))		 
+	return render_to_response('landing.html', context)
