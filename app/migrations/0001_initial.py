d497e557a13951f805a31ed6b949d53e8a9d9fba
Vipul Sharma <vipul.sharma20@gmail.com>
2016-02-07 16:13:44 +0530

    added migrations

---
+++ app/migrations/0001_initial.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+import datetime
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Query',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('name', models.CharField(max_length=100)),
+                ('email', models.EmailField(max_length=254)),
+                ('message', models.CharField(max_length=255)),
+                ('pub_date', models.DateTimeField(default=datetime.datetime.now, blank=True)),
+            ],
+        ),
+    ]
